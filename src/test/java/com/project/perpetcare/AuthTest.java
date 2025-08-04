c57a831368fec575fd7f96a0ca16f3a9d08dfae1
cheny <cheyeon001214@naver.com>
2025-08-04 15:49:27 +0900

    feat: AuthTest, dto package

--- src/test/java/com/project/perpetcare/AuthTest.java
+++ src/test/java/com/project/perpetcare/AuthTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.Reader;
 
-public class UserTest {
+public class AuthTest {
     @Test
     public void insertUser(){
         User user = new User("codus@naver.com", "곽채연", "2000-12-14", "f", "1234", "01055821857", Grade.bronze, 1);
