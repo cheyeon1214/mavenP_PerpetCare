a8f553da95d0e317b3d6dccd8fc3c22d3377ff4f
cheny <cheyeon001214@naver.com>
2025-08-04 23:49:28 +0900

    feat: addPostPet, updateOpening create & Test OK

--- src/test/java/com/project/perpetcare/ApplyTest.java
+++ src/test/java/com/project/perpetcare/ApplyTest.java
@@ -13,13 +13,14 @@ import java.sql.Array;
 import java.sql.Blob;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ApplyTest {
     @Test
     public void addOpening() throws Exception{
         ArrayList<Pet> pets = new ArrayList<>();
 
-        pets.add(new Pet("codus@naver.com", 1, "romi", "고양이", "코숏", LocalDateTime.now(), "f", null));
+        pets.add(new Pet(1, "codus@naver.com", "romi", "고양이", "코숏", LocalDateTime.now(), "f", null));
         Opening opening = new Opening(0, "codus@naver.com", LocalDateTime.now(), "와주세요", LocalDateTime.now(),  LocalDateTime.of(2025, 8, 10, 2, 10, 00), "12000", "시급", "서초구 방배동", "동물을 좋아하는 사람", "저희는 홈캠이 있습니다.", false, pets);
 
         Reader r = Resources.getResourceAsReader("config/SqlMapConfig.xml");
@@ -32,4 +33,37 @@ public class ApplyTest {
 
         System.out.println("Insert result: " + result);
     }
+
+    @Test
+    public void addPostPet() throws Exception{
+        ArrayList<Pet> pets = new ArrayList<>();
+//        pets.add(new Pet(1, "codus@naver.com", "romi", "고양이", "코숏", LocalDateTime.now(), "f", null));
+        Reader r = Resources.getResourceAsReader("config/SqlMapConfig.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(r);
+        SqlSession session = factory.openSession();
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("oNo", 2);
+        map.put("pNo", 1);
+        int result = session.insert("ns.sql.OpeningMapper.addPostPet", map);
+
+        session.commit();
+        session.close();
+
+        System.out.println("Insert result: " + result);
+    }
+
+    @Test
+    public void updateOpening() throws Exception{
+
+        Opening opening = new Opening(2, "codus@naver.com", LocalDateTime.now(), "맡아주세요", LocalDateTime.now(),  LocalDateTime.of(2025, 8, 11, 2, 30, 00), "12000", "시급", "서초구 방배3동", "동물을 좋아하는 사람", "저희는 홈캠이 있습니다.", false, null);
+        Reader r = Resources.getResourceAsReader("config/SqlMapConfig.xml");
+        SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(r);
+        SqlSession session = factory.openSession();
+        int result = session.update("ns.sql.OpeningMapper.updateOpening", opening);
+        session.commit();
+        session.close();
+        System.out.println("Update result: " + result);
+
+    }
+
 }
