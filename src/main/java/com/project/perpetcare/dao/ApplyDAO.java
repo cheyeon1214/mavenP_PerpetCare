4f18202a419b500cbb3f1759c8d9fdb3565b8d0e
cheny <cheyeon001214@naver.com>
2025-08-05 11:45:42 +0900

    feat: ApplyDAO create

--- src/main/java/com/project/perpetcare/dao/ApplyDAO.java
+++ src/main/java/com/project/perpetcare/dao/ApplyDAO.java
@@ -1,8 +1,12 @@
 package com.project.perpetcare.dao;
 
+import com.project.perpetcare.domain.enums.ApplyStatus;
+import com.project.perpetcare.dto.ApplyUserDTO;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
 
 @Repository
 public class ApplyDAO {
@@ -10,6 +14,21 @@ public class ApplyDAO {
 
     @Autowired
     private SqlSession sqlSession;
+
+    public void applyToOpening() throws Exception{
+        sqlSession.insert(NS+"applyToOpening");
+    }
+
+    public ApplyUserDTO getApplicants() throws Exception{
+        return sqlSession.selectOne(NS+"getApplicants");
+    }
+
+    public void updateApplyStatus(ApplyStatus status, Integer no) throws Exception{
+        HashMap<String,Object> map = new HashMap<>();
+        map.put("status",status);
+        map.put("no",no);
+        sqlSession.update(NS+"updateApplyStatus",map);
+    }
 
 
 }
