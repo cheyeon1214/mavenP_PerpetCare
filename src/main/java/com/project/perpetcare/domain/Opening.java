bf62f4dde69dc362cc438dbb37ae059d1325dbe0
yjungs2 <100326172+yjungs2@users.noreply.github.com>
2025-08-05 10:40:05 +0900

    feat: create dto class

--- src/main/java/com/project/perpetcare/domain/Opening.java
+++ src/main/java/com/project/perpetcare/domain/Opening.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Opening {
     private int no; //o_no
     private String uEmail; //u_email User의 외래키
-    private LocalDateTime createAt; //o_createdAt
+    private LocalDateTime createdAt; //o_createdAt
     private String careWay; //o_careway
     private LocalDateTime sDate; //o_sdate
     private LocalDateTime eDate; //o_edate
