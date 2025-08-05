bbc825e125a1f5a634225359d5cf27d5c4c0fdb4
kahyun <ahdalahdahd@gmail.com>
2025-08-05 10:41:01 +0900

    feat: pet back-end

--- src/main/java/com/project/perpetcare/dao/PetDAO.java
+++ src/main/java/com/project/perpetcare/dao/PetDAO.java
@@ -1,8 +1,11 @@
 package com.project.perpetcare.dao;
 
+import com.project.perpetcare.domain.Pet;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class PetDAO {
@@ -11,5 +14,17 @@ public class PetDAO {
     @Autowired
     private SqlSession sqlSession;
 
+    public void insertPet(Pet pet)throws Exception{
+        sqlSession.insert(NS+"insertPet", pet);
+    }
+    public void updatePet(Pet pet)throws Exception{
+        sqlSession.update(NS+"updatePet",pet);
+    }
+    public void deletePet(int no)throws Exception{
+        sqlSession.delete(NS+"deletePet",no);
+    }
+    public List<Pet> selectPet(String uEmail)throws Exception{
+        return sqlSession.selectList(NS+"selectPet",uEmail);
+    }
 
 }
--- src/main/java/com/project/perpetcare/service/PetService.java
+++ src/main/java/com/project/perpetcare/service/PetService.java
@@ -2,8 +2,11 @@ package com.project.perpetcare.service;
 
 import com.project.perpetcare.dao.PetDAO;
 import com.project.perpetcare.dao.ProfileDAO;
+import com.project.perpetcare.domain.Pet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PetService {
@@ -11,4 +14,16 @@ public class PetService {
     @Autowired
     private PetDAO petDAO;
 
+    public void insertPet(Pet pet)throws Exception{
+        petDAO.insertPet(pet);
+    }
+    public void updatePet(Pet pet)throws Exception{
+        petDAO.updatePet(pet);
+    }
+    public void deletePet(int no)throws Exception{
+        petDAO.deletePet(no);
+    }
+    public List<Pet> selectPet(String uEmail)throws Exception{
+        return petDAO.selectPet(uEmail);
+    }
 }
