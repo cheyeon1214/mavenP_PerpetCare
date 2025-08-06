7bfd6b836c35329d35ee945619f5a212021d92e5
kahyun <ahdalahdahd@gmail.com>
2025-08-06 09:59:57 +0900

    feat: 오류 수정

--- src/main/java/com/project/perpetcare/controller/PetController.java
+++ src/main/java/com/project/perpetcare/controller/PetController.java
@@ -1,13 +1,14 @@
 package com.project.perpetcare.controller;
 
+import com.project.perpetcare.domain.Pet;
 import com.project.perpetcare.service.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 //@RequestMapping("/pet")
