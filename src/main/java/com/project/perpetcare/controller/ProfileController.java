35c324d98e9d93b1744ff0818ec78ea53ca88cac
이태란 <dalbogi5@gmail.com>
2025-08-07 16:52:34 +0900

    feat:experiencePage

--- src/main/java/com/project/perpetcare/controller/ProfileController.java
+++ src/main/java/com/project/perpetcare/controller/ProfileController.java
@@ -1,14 +1,23 @@
 package com.project.perpetcare.controller;
 
+import com.project.perpetcare.dao.ProfileDAO;
 import com.project.perpetcare.service.AuthService;
 import com.project.perpetcare.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController {
 
     @Autowired
     private ProfileService profileService;
+    @Autowired
+    private ProfileDAO profileDAO;
 
+    @GetMapping("/experiencePage")
+    public String getProfile(){
+        return "profilePage/experiencePage";
+    }
 }
